b17c3f7b857bd68fb071a693fcfbdc5fc3100d59
shun tanaka <shun.tanaka980520@gmail.com>
2023-08-26 16:59:30 +0900

    構造体UserNamenの作成

--- api/src/domain.rs
+++ api/src/domain.rs
@@ -1,1 +1,19 @@
 pub mod tests;
+
+#[allow(dead_code)]
+struct UserName {
+    value: String,
+}
+
+impl UserName {
+    #[allow(dead_code)]
+    fn new(s: &str) -> Result<UserName, String> {
+        if s == "" {
+            return Err("空文字です".to_string());
+        }
+
+        Ok(UserName {
+            value: s.to_string(),
+        })
+    }
+}
--- api/src/domain/tests.rs
+++ api/src/domain/tests.rs
@@ -1,3 +1,3 @@
-pub fn health_check(){
+pub fn health_check() {
     println!("module is succeed!!");
 }
