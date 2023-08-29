a9a6241987a02a8b69533cfddfa4f84dc832f19b
Kota <yuzukh0211@gmail.com>
2023-08-29 17:09:05 +0900

    remove old code

--- api/src/domain.rs
+++ api/src/domain.rs
@@ -1,20 +1,2 @@
 pub mod tests;
 pub mod user_name;
-
-#[allow(dead_code)]
-pub struct UserName {
-    value: String,
-}
-
-impl UserName {
-    #[allow(dead_code)]
-    pub fn new(s: &str) -> Result<UserName, String> {
-        if s == "" {
-            return Err("空文字です".to_string());
-        }
-
-        Ok(UserName {
-            value: s.to_string(),
-        })
-    }
-}
