0f0d45c1625677ca2ad0858fd263ab849d2455c9
shun tanaka <shun.tanaka980520@gmail.com>
2023-09-06 00:45:10 +0900

    氏名入力条件・テスト追加

--- api/src/domain/user_name.rs
+++ api/src/domain/user_name.rs
@@ -10,8 +10,17 @@ pub struct UserName {
 impl UserName {
     #[allow(dead_code)]
     pub fn new(s: &str) -> Result<UserName, String> {
+        // 空文字の場合
         if s == "" {
             return Err("空文字です".to_string());
+        }
+
+        // 半角英数記号が含まれる場合
+        use regex::Regex;
+        let regex_half_width = Regex::new(r"^[ -~]*$").unwrap();
+        
+        if regex_half_width.is_match(s){
+            return Err("半角の文字が含まれています".to_string());
         }
 
         Ok(UserName {
--- api/src/domain/user_name/tests.rs
+++ api/src/domain/user_name/tests.rs
@@ -6,4 +6,11 @@ pub mod error{
         let result = UserName::new(empty);
         assert_eq!(result,Err("空文字です".to_string()));
     }
+
+    #[test]
+    pub fn half_width_value(){
+        let half_width="abcde";
+        let result = UserName::new(half_width);
+        assert_eq!(result,Err("半角の文字が含まれています".to_string()));
+    }
 }
