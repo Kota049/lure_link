94fa9c7d4b18c6a2e97daf05915e6a396177bd93
shun tanaka <shun.tanaka980520@gmail.com>
2023-09-07 23:48:31 +0900

    氏名入力条件追加

--- api/src/domain/user_name/tests.rs
+++ api/src/domain/user_name/tests.rs
@@ -8,9 +8,30 @@ pub mod error{
     }
 
     #[test]
-    pub fn half_width_value(){
-        let half_width="abcde";
-        let result = UserName::new(half_width);
-        assert_eq!(result,Err("半角の文字が含まれています".to_string()));
+    pub fn half_width_num_value(){
+        let half_width_num="1234";
+        let result = UserName::new(half_width_num);
+        assert_eq!(result,Err("半角数字が含まれています".to_string()));
+    }
+
+    #[test]
+    pub fn full_width_num_value(){
+        let full_width_num = "１２３４";
+        let result = UserName::new(full_width_num);
+        assert_eq!(result,Err("全角数字が含まれています".to_string()));
+    }
+
+    #[test]
+    pub fn full_width_sym_value(){
+        let sym = "★";
+        let result = UserName::new(sym);
+        assert_eq!(result,Err("全角記号が含まれています".to_string()));
+    }
+
+    #[test]
+    pub fn length_limit(){
+        let length_limit_value = "あいうえおかきくけこさしすせそたちつてとなにぬねのはひふ";
+        let result = UserName::new(length_limit_value);
+        assert_eq!(result,Err("26字以内に収めてください".to_string()));
     }
 }
