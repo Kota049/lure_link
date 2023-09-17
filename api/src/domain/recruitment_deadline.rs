8082a34121731606592a2780f8f550a26daa2db0
Kota <yuzukh0211@gmail.com>
2023-09-17 23:56:24 +0900

    green invalid

--- api/src/domain/recruitment_deadline.rs
+++ api/src/domain/recruitment_deadline.rs
@@ -1,3 +1,5 @@
+use crate::utils::validate::date::validate_date;
+
 #[cfg(test)]
 mod tests;
 
@@ -8,6 +10,9 @@ pub struct RecruitmentDeadline {
 
 impl RecruitmentDeadline {
     pub fn new(deadline: &str) -> Result<Self, String> {
+        if validate_date(deadline).is_err() {
+            return Err("不正なrecruitment_deadlineです".to_string());
+        }
         Ok(RecruitmentDeadline {
             inner: deadline.to_string(),
         })
