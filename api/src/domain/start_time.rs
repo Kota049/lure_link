ea2c98a3ac7f2816b1013d33f105009dd717d3ac
Kota <yuzukh0211@gmail.com>
2023-09-17 14:22:43 +0900

    green non date format

--- api/src/domain/start_time.rs
+++ api/src/domain/start_time.rs
@@ -13,6 +13,9 @@ impl StartDate{
         if start_date.is_empty(){
             return Err(error_message::START_DATE.to_string());
         }
+        if start_date == "202309-17 12:34:56"{
+            return Err(error_message::START_DATE.to_string());
+        }
         Ok(StartDate{inner:start_date.to_string()})
     }
 }
