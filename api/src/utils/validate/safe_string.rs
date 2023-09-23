8b6197c8425f74f13e873b0d5889cf60f7df962a
Kota <yuzukh0211@gmail.com>
2023-09-23 22:40:26 +0900

    refactor extract method

--- api/src/utils/validate/safe_string.rs
+++ api/src/utils/validate/safe_string.rs
@@ -1,4 +1,9 @@
 pub fn validate_safe_string(s: &str) -> Result<(), String> {
+    is_empty(s)?;
+    Ok(())
+}
+
+fn is_empty(s: &str) -> Result<(), String> {
     if s.is_empty() {
         return Err("不正な文字列です".to_string());
     }
@@ -7,7 +12,7 @@ pub fn validate_safe_string(s: &str) -> Result<(), String> {
 
 #[cfg(test)]
 mod tests {
-    use crate::utils::validate::safe_string::validate_safe_string;
+    use super::*;
 
     #[test]
     fn valid() {
