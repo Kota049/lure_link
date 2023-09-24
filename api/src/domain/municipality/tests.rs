b54fb4529d6e54dd6464592404ada3a1e899dad9
Kota <yuzukh0211@gmail.com>
2023-09-24 22:07:41 +0900

    refactor extract const

--- api/src/domain/municipality/tests.rs
+++ api/src/domain/municipality/tests.rs
@@ -1,13 +1,15 @@
 use super::*;
 
+const VALID_MUNICIPALITY: &'static str = "川越市";
+
 #[test]
 fn valid() {
-    let municipality = "川越市";
+    let municipality = VALID_MUNICIPALITY;
     let result = Municipality::new(municipality);
     assert_eq!(
         municipality,
         Ok(Municipality {
-            inner: "川越市".to_string()
+            inner: VALID_MUNICIPALITY.to_string()
         })
     );
 }
