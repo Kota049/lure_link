3e1e5f3630b139c7b069b6942db4a2f11791fb9b
Kota <yuzukh0211@gmail.com>
2023-09-24 23:32:38 +0900

    red non_integer

--- api/src/domain/id/tests.rs
+++ api/src/domain/id/tests.rs
@@ -6,3 +6,10 @@ fn valid() {
     let result = ID::new(id);
     assert_eq!(result, Ok(ID { inner: 11111 }));
 }
+
+#[test]
+fn non_integer() {
+    let non_integer = "a";
+    let result = ID::new(non_integer);
+    assert_eq!(result, Err("不正なidです".to_string()));
+}
