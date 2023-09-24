0aa4a39c9f78885bf9755449138cfe3c0ea35b25
Kota <yuzukh0211@gmail.com>
2023-09-24 22:25:45 +0900

    red non_integer

--- api/src/domain/budget/tests.rs
+++ api/src/domain/budget/tests.rs
@@ -6,3 +6,10 @@ fn valid() {
     let result = Budget::new(&budget);
     assert_eq!(result, Ok(Budget { inner: 1000 }));
 }
+
+#[test]
+fn non_integer() {
+    let non_integer = "a";
+    let result = Budget::new(non_integer);
+    assert_eq!(result, Err("不正なbudgetです".to_string()));
+}
