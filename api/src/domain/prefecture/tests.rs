f1385c926b1506945c585c8c35abf68097fc2e0c
Kota <yuzukh0211@gmail.com>
2023-09-24 21:52:27 +0900

    red to_string

--- api/src/domain/prefecture/tests.rs
+++ api/src/domain/prefecture/tests.rs
@@ -18,3 +18,13 @@ fn invalid() {
     let result = Prefecture::new(prefecture);
     assert_eq!(result, Err(INVALID_PREFECTURE_MESSAGE.to_string()));
 }
+
+#[test]
+fn to_string() {
+    let prefecture = Prefecture {
+        inner: "埼玉県".to_string(),
+    };
+    let result = prefecture.to_string();
+
+    assert_eq!(result, "埼玉県");
+}
