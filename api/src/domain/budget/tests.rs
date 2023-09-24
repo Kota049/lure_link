9e9b43e3c4fe8376d14c97ae10f92939aa8db106
Kota <yuzukh0211@gmail.com>
2023-09-24 22:29:56 +0900

    red out of range

--- api/src/domain/budget/tests.rs
+++ api/src/domain/budget/tests.rs
@@ -13,3 +13,9 @@ fn non_integer() {
     let result = Budget::new(non_integer);
     assert_eq!(result, Err(INVALID_BUDGET_MESSAGE.to_string()));
 }
+#[test]
+fn out_of_range() {
+    let out_of_range = 100001.to_string();
+    let result = Budget::new(&out_of_range);
+    assert_eq!(result, Err(INVALID_BUDGET_MESSAGE.to_string()));
+}
