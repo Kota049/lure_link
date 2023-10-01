0cb33319aa144c02abd7e15a18f3353ec0db9372
Kota <yuzukh0211@gmail.com>
2023-10-01 15:25:01 +0900

    red db_error

--- api/src/db/read/recruitment_summary/tests.rs
+++ api/src/db/read/recruitment_summary/tests.rs
@@ -1,5 +1,6 @@
 use super::*;
 use crate::db::connection::connection;
+use tokio_postgres::NoTls;
 
 #[tokio::test]
 async fn get_recruitments() {
@@ -8,4 +9,12 @@ async fn get_recruitments() {
     let result = get_recruitment_summary_list(&client).await;
 
     assert!(result.is_ok());
+}
+#[tokio::test]
+async fn db_error() {
+    let (client, _) = tokio_postgres::connect("", NoTls).await.unwrap();
+
+    let result = get_recruitment_summary_list(&client).await;
+
+    assert_eq!(result, Err("データが取得できませんでした".to_string()));
 }
