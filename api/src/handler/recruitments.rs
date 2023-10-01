66d9a626870ede891f7234c9a8ba9a5331708c40
Kota <yuzukh0211@gmail.com>
2023-10-01 19:18:21 +0900

    green valid response

--- api/src/handler/recruitments.rs
+++ api/src/handler/recruitments.rs
@@ -1,6 +1,9 @@
 use crate::db::app_state::ClientState;
+use crate::db::read::recruitment_summary::get_recruitment_summary_list;
 use axum::extract::State;
-use axum::response::Response;
+use axum::http::StatusCode;
+use axum::response::{IntoResponse, Response};
+use serde_json::{json, Value};
 use std::sync::Arc;
 
 #[cfg(test)]
@@ -9,5 +12,16 @@ mod tests;
 pub async fn read_recruitments(
     State(state): State<Arc<ClientState>>,
 ) -> Result<Response, Response> {
-    todo!()
+    let primitive_recruitments = get_recruitment_summary_list(&state.client).await.unwrap();
+
+    let mut result: Vec<Value> = Vec::new();
+    for recruitment in primitive_recruitments {
+        let recruitment = recruitment.sophisticate();
+        if recruitment.is_err() {
+            continue;
+        }
+        let recruitment = recruitment.unwrap();
+        result.push(recruitment.to_value());
+    }
+    Ok((StatusCode::OK, json!(result).to_string()).into_response())
 }
