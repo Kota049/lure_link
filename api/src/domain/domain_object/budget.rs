5299259d59b08166da81196bce86b3abbdedb18d
Kota <yuzukh0211@gmail.com>
2024-01-01 01:36:01 +0900

    extract const

--- api/src/domain/domain_object/budget.rs
+++ api/src/domain/domain_object/budget.rs
@@ -1,7 +1,9 @@
 use serde::Serialize;
 use crate::impl_integer_rule;
 
+const BUDGET_MAX: i32 = 1000000;
+
 #[derive(Debug, Clone, Serialize, PartialOrd, PartialEq)]
 pub struct Budget(i32);
 
-impl_integer_rule!(Budget,1000000);
+impl_integer_rule!(Budget,BUDGET_MAX);
--- api/src/domain/domain_object/fishing_point.rs
+++ api/src/domain/domain_object/fishing_point.rs
@@ -1,7 +1,7 @@
 use serde::Serialize;
 use crate::impl_text_rule;
-
+const MAX_POINT_NAME:usize = 30usize;
 #[derive(Debug, Clone, Serialize, PartialOrd, PartialEq)]
-pub struct Destination(String);
+pub struct PointName(String);
 
-impl_text_rule!(Destination,30usize);
+impl_text_rule!(PointName,MAX_POINT_NAME);
