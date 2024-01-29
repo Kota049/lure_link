a4009b49686aad3f1b401dab61deb784a66c1750
Kota <yuzukh0211@gmail.com>
2024-01-29 22:15:50 +0900

    green test_validate_first_register_user

--- api/src/service/validation.rs
+++ api/src/service/validation.rs
@@ -5,5 +5,27 @@ use crate::use_case::user_use_case::dto::UpdateUser;
 mod tests;
 
 pub fn validate_first_register_user(u: &UpdateUser) -> Result<(), Error> {
-    todo!()
+    if u.first_name.is_none() {
+        return Err(Error::ValidateError("名前が登録されていません".to_string()));
+    }
+    if u.first_name_jp.is_none() {
+        return Err(Error::ValidateError(
+            "名前(フリガナ)が登録されていません".to_string(),
+        ));
+    }
+    if u.last_name.is_none() {
+        return Err(Error::ValidateError("名字が登録されていません".to_string()));
+    }
+    if u.last_name_jp.is_none() {
+        return Err(Error::ValidateError(
+            "名字(フリガナ)が登録されていません".to_string(),
+        ));
+    }
+    if u.nick_name.is_none() {
+        return Err(Error::ValidateError(
+            "ニックネームが登録されていません".to_string(),
+        ));
+    }
+
+    Ok(())
 }
