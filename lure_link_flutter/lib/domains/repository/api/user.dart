2a8e6a5ca3f7820f282906d1a0b931a6aad1a442
Kota <yuzukh0211@gmail.com>
2024-02-04 23:42:59 +0900

    modify arg

--- lure_link_flutter/lib/domains/repository/api/user.dart
+++ lure_link_flutter/lib/domains/repository/api/user.dart
@@ -2,7 +2,7 @@ import 'package:dartz/dartz.dart';
 import '../../entity/user.dart';
 
 abstract interface class UserRepository {
-  Future<Either<User, Error>> login();
+  Future<Either<User, Error>> login(String lineToken);
 
-  Future<Either<User, Error>> updateUser();
+  Future<Either<User, Error>> updateUser(User user);
 }
