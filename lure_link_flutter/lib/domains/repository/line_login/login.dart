271c86e6ab06c213929850910dfa90da69d9d1b5
Kota <yuzukh0211@gmail.com>
2024-02-05 21:51:59 +0900

    wip line login repository

---
+++ lure_link_flutter/lib/domains/repository/line_login/login.dart
@@ -0,0 +1,23 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_line_sdk/flutter_line_sdk.dart';
+import 'package:lure_link_flutter/domains/value_object/custom_error.dart';
+
+abstract interface class LineLoginRepository {
+  Future<Either<String, CustomError>> login();
+}
+
+// =======================concrete
+
+class LineLogin implements LineLoginRepository {
+  @override
+  Future<Either<String, CustomError>> login() async {
+    // todo: LineAPIへのアクセス
+    try {
+      final result = await LineSDK.instance.login();
+      final accecc_token = result.accessToken.toString();
+      return Left(accecc_token);
+    } catch (_) {
+      return Right(CustomError("failed to login to LINE"));
+    }
+  }
+}
