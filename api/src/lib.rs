5afa6cf0b1ee4c20518bf608996e43a337214019
Kota <yuzukh0211@gmail.com>
2024-05-27 23:31:16 +0900

    [Refactor] dead code

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -2,11 +2,9 @@ pub mod constants;
 pub mod db;
 pub mod domain;
 pub mod entity;
-pub mod handler;
 pub mod service;
 
 pub mod error;
-
 pub mod infrastructure;
 pub mod repository;
 pub mod use_case;
